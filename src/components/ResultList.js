f88cb50c682225e8d75b30101d7ee575de80ebc3
Hotmonk <avinashkhetri@gmail.com>
2020-04-30 17:38:59 +0530

    result show screen made and navigation set

--- src/components/ResultList.js
+++ src/components/ResultList.js
@@ -1,17 +1,24 @@
 import React from 'react';
-import {View, Text, StyleSheet, FlatList} from 'react-native';
+import {View, Text, StyleSheet, FlatList, TouchableOpacity} from 'react-native';
 import ResultsDetail from './ResultsDetail';
+import {withNavigation} from 'react-navigation';
 
-const ResultList=({title, results}) =>{
-    return <View>
+
+const ResultList=({title, results, navigation}) =>{
+    return <View style={styles.container}>
         <Text style={styles.title}>{title}</Text>
         {/* <Text>Results: {results.length}</Text> */}
         <FlatList
         horizontal
         data={results}
         keyExtractor={(result)=>result.id}
+        showsHorizontalScrollIndicator={false}
         renderItem={({item})=>{
-        return <ResultsDetail result={item}/>
+        return (
+        <TouchableOpacity onPress={()=>navigation.navigate('ResultsShow')}>
+        <ResultsDetail result={item}/>
+        </TouchableOpacity>
+        )
         }}
         />
     </View>
@@ -19,10 +26,15 @@ const ResultList=({title, results}) =>{
 
 const styles=StyleSheet.create({
     title: {
+        marginLeft:15,
         fontSize: 18,
-        fontWeight: 'bold'
+        fontWeight: 'bold',
+        marginBottom: 5
+    },
+    container:{
+        marginBottom:10
     }
 });
 
 
-export default ResultList;
+export default withNavigation(ResultList);
--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,5 +1,5 @@
 import React, {useState} from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import {View, Text, StyleSheet, ScrollView} from 'react-native';
 import SearchBar from '../components/SearchBar';
 import useResults from '../hooks/useResults';
 import ResultList from '../components/ResultList';
@@ -14,16 +14,20 @@ const SearchScreen=()=>{
     }
     return (
     <View style={styles.bg}>
+        {/* or you can add emptt tag as well */}
         <SearchBar 
             term={term} 
             onTermChange={(newTerm)=>setTerm(newTerm)}
             onTermSubmit={()=>searchApi(term)}
         />
         {errorMessage? <Text>{errorMessage}</Text>: null}
-        <Text>We have found {results.length} results</Text>
+        {/* <Text>We have found {results.length} results</Text> */}
+        <ScrollView>
         <ResultList results={filterResultsByPrice('$')} title="Cost Effective"/>
         <ResultList results={filterResultsByPrice('$$')} title="Bit Pricier"/>
         <ResultList results={filterResultsByPrice('$$$')} title="Big Spender"/>
+        </ScrollView>
+
     </View>
     );
 }
@@ -32,6 +36,7 @@ const SearchScreen=()=>{
 const styles=StyleSheet.create({
     bg: {
         backgroundColor: 'white',
+        flex:1
     }
 });
 
